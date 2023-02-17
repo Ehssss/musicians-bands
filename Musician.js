f7ff73dd0e47361b7754d994a5b6ac3deb36eaad
Ehsaan Alam <ehsaanalam0809@gmail.com>
2023-02-17 10:32:11 +0000

    done most of the TODOs, just need to sort out RUNNING the tests

--- Musician.js
+++ Musician.js
@@ -1,8 +1,15 @@
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Musician model
-let Musician;
+const { DataTypes } = require('sequelize');
+const sequelize = require('db.js');
 
+const Musician = sequelize.define('Musician', {
+    name: DataTypes.STRING,
+    instrument: DataTypes.STRING
+});
 module.exports = {
     Musician
 };
+
+module.exports = Musician;
